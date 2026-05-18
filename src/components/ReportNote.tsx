75f1ca6a95c692bd0ce29fa76eb5bf25f9bd48e8
mipa509 <mikolaj.pawelczak@gmail.com>
2026-05-18 01:15:41 +0100

    feat(report): show vehicle side elevation in Loads section

--- src/components/ReportNote.tsx
+++ src/components/ReportNote.tsx
@@ -11,6 +11,7 @@ import {
   resolveSectionAxis,
 } from "../app/sectionCurve";
 import { SectionPlot } from "./SectionPlot";
+import { VehicleSideElevation } from "./VehicleSideElevation";
 
 interface ReportNoteProps {
   model: SlabModel;
@@ -264,6 +265,8 @@ export const ReportNote = ({ model, results, images, preparedBy }: ReportNotePro
             </tbody>
           </table>
         )}
+        <h3>Side elevation</h3>
+        <VehicleSideElevation vehicle={model.vehicle} />
       </section>
 
       <section className="report-note-section">
